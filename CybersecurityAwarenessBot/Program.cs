5e6d3ca808b70fde088beacff9596dfbbd78e9e1
lindokuhlemtshalibj <lindokuhlemtshalibj@gmail.com>
2026-04-09 12:43:41 +0200

    Added Project folders and class files

--- CybersecurityAwarenessBot/Program.cs
+++ CybersecurityAwarenessBot/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Program.cs
+// Entry point for the Cybersecurity Awareness Bot application.
+// This file keeps things simple - it just starts the chatbot.
+// Written by: [lindokuhle Mtshali ]
+// Student Number: [ST10233093]
+// Date: April 2026
+
+using System;
+
+namespace CybersecurityAwarenessBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Play the voice greeting when the app launches
+            VoiceGreeting.Play();
+
+            // Start the chatbot
+            ChatBot bot = new ChatBot();
+            bot.Start();
+        }
+    }
+}
